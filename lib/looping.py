041172eee72ce4a0ef768137313c305fbdf866a5
Kyle Greenidge <kylegreen149@gmail.com>
2024-08-26 13:21:18 -0400

    Done

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    count = 11
+    while count > 0:
+        count -= 1
+        print(count)
+        print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared = []
+    for num in int_list:
+        squared_num = num * num
+        squared.append(squared_num)  
+    return squared
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    count = 0
+    while count < 100:
+        count += 1
+        if count % 3 == 0 and count % 5 == 0:
+            print("FizzBuzz")
+        elif count % 3 == 0:
+            print("Fizz")
+        elif count % 5 == 0:
+            print("Buzz")
+        else:
+            print(count)
